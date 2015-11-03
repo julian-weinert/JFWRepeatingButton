5af9e2cea1d23b45d35f9af1ad2c5da23e8fcf63
Julian Weinert <julian-weinert@users.noreply.github.com>
2015-11-03 12:28:54 +0100

    Create JFWRepeatingButton.m

---
+++ JFWRepeatingButton.m
@@ -0,0 +1,99 @@
+//
+//  JFWRepeatingButton.m
+//
+//  Created by Julian Weinert on 12/03/15.
+//  Copyright (c) 2015 Julian Weinert Softwareentwicklung. All rights granted.
+//
+
+#import "JFWRepeatingButton.h"
+
+@interface JFWRepeatingButton ()
+
+@property (nonatomic, retain) NSTimer *delayTimer;
+@property (nonatomic, retain) NSTimer *repeatingTimer;
+@property (nonatomic, retain) NSTimer *accellerationTimer;
+
+@property (atomic, assign) BOOL eventReactionPaused;
+
+@end
+
+@implementation JFWRepeatingButton
+
++ (id)buttonWithType:(UIButtonType)buttonType {
+	return [[self alloc] init];
+}
+
+- (instancetype)init {
+	if (self = [super init]) {
+		[self setup];
+	}
+	return self;
+}
+
+- (void)awakeFromNib {
+	[self setup];
+}
+
+- (void)setup {
+	[self addTarget:self action:@selector(clickBegan:) forControlEvents:UIControlEventTouchDown];
+	[self addTarget:self action:@selector(clickEnded) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)startRepeating {
+	[[self delayTimer] invalidate];
+	_delayTimer = nil;
+	
+	_repeatingTimer = [NSTimer scheduledTimerWithTimeInterval:[self repeatInterval]
+													   target:self
+													 selector:@selector(repeat)
+													 userInfo:nil
+													  repeats:YES];
+}
+
+- (void)stopRepeating {
+	[[self delayTimer] invalidate];
+	_delayTimer = nil;
+	
+	[[self repeatingTimer] invalidate];
+	_repeatingTimer = nil;
+}
+
+- (void)repeat {
+	_eventReactionPaused = YES;
+	[self sendActionsForControlEvents:UIControlEventTouchUpInside];
+	_eventReactionPaused = NO;
+}
+
+- (void)clickBegan:(id)sender {
+	[self stopRepeating];
+	
+	if ([self repeatInterval] > 0) {
+		_delayTimer = [NSTimer scheduledTimerWithTimeInterval:[self threshhold]
+													   target:self
+													 selector:@selector(startRepeating)
+													 userInfo:nil
+													  repeats:NO];
+	}
+	
+	if ([self accellerationInterval] > 0) {
+		
+	}
+}
+
+- (void)clickEnded {
+	if ([self eventReactionPaused]) {
+		return;
+	}
+	
+	[self stopRepeating];
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
